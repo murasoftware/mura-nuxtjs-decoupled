d96962176ccabe87659925875b806346142f5cef
Matt Levine <matt.levine@blueriver.com>
2019-02-23 12:15:58 -0800

    Switching to using boostrap-vue components

--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -2,26 +2,28 @@
 <div>
 <script>window.queuedMuraCmds=[],window.queuedMuraPreInitCmds=[],window.mura=window.m=window.Mura=function(u){window.queuedMuraCmds.push(u)},window.Mura.preInit=function(u){window.queuedMuraPreInitCmds.push(u)};</script>
 <div>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Mura/NuxtJS</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto" v-if="primaryNavData">
-      <li v-bind:class="{ 'active': (content.contentid==item.contentid), 'nav-item':true }" v-for="item of primaryNavData" :key="item.contentid">
-				<a class="nav-link"v-bind:href="item.url">{{item.menutitle}}<!--<span class="sr-only">(current)</span>--></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" name="keywords" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			<input name="display" type="hidden" value="search"/>
-			<input name="nocache" type="hidden" value="1"/>
-			<input name="newsearch" type="hidden" value="1"/>
-		</form>
-  </div>
-</nav>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Mura/NuxtJS</b-navbar-brand>
+
+    <b-navbar-toggle target="nav_collapse" />
+
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item v-bind:class="{ 'active': (content.contentid==item.contentid)}" v-for="item of primaryNavData" :key="item.contentid" v-bind:href="item.url">{{item.menutitle}}</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" name="keywords" class="mr-sm-2" type="text" placeholder="Search" />
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+					<input name="display" type="hidden" value="search"/>
+					<input name="nocache" type="hidden" value="1"/>
+					<input name="newsearch" type="hidden" value="1"/>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </div>
 <h1>{{content.title}}</h1>
 <div v-html="content.body"></div>
