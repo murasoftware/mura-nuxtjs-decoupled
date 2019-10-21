5e5ad4bbb6e8b8a6095ccb3db078bba82d9e5717
Matt Levine <matt.levine@blueriver.com>
2019-10-21 12:39:37 -0700

    Updating to Mura 8

--- nuxtjs/components/modules/CollectionLayout.vue
+++ nuxtjs/components/modules/CollectionLayout.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 	<ul>
-		<li v-for="item in context.collection.get('items')">
+		<li v-for="item in context.collection.get('items')" v-bind:key="item">
 			<a v-bind:href="item.get('url')">{{item.get('menutitle')}}</a>
 		</li>
 	</ul>
